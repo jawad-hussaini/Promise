a16bde3108848eb9bbe0fb30a41feee9d125a46e
Syed Jawad <jawadhussaini1804@gmail.com>
2024-06-01 16:30:02 +0500

    Promises

--- promise.js
+++ promise.js
@@ -1,10 +1,22 @@
-async function DisplayData() {
-    let myProfile = new Promise(function(resolve, reject){
+// async function DisplayData() {
+//     let myProfile = new Promise(function(resolve, reject){
+//         setTimeout(()=>{
+//             resolve("Hello")
+//         }, 2000)
+//     });
+//     let fullFilled = await myProfile;
+//     console.log(fullFilled)
+// }
+// DisplayData();
+
+async function Data() {
+let again = new Promise(function(resolve){
         setTimeout(()=>{
-            resolve("Hello")
-        }, 2000)
-    });
-    let fullFilled = await myProfile;
-    console.log(fullFilled)
+            resolve("Helloooooooo")
+        }, 1500);
+    })
+
+ let werg = await again;
+ console.log(werg)
 }
-DisplayData()
+Data();
