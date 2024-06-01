119e95149389329d34ca65fd04165190fceef578
Syed Jawad <jawadhussaini1804@gmail.com>
2024-06-01 19:14:06 +0500

    Promise

--- promise.js
+++ promise.js
@@ -9,14 +9,29 @@
 // }
 // DisplayData();
 
-async function Data() {
-let again = new Promise(function(resolve){
-        setTimeout(()=>{
-            resolve("Helloooooooo")
-        }, 1500);
-    })
+// async function Data() {
+// let again = new Promise(function(resolve){
+//         setTimeout(()=>{
+//             resolve("Helloooooooo")
+//         }, 1500);
+//     })
 
- let werg = await again;
- console.log(werg)
-}
-Data();
+//  let werg = await again;
+//  console.log(werg)
+// }
+// Data();
+
+function pro(){
+    let promised = (function(resolve, reject){
+        setTimeout(()=>{
+            if(resolve){
+            console.log("You are on time");
+            }else{
+                console.log(reject) 
+            }
+        },2200)
+    });
+    let promiseDone = promised;
+    console.log(promiseDone)
+};
+pro();
