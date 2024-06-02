e50aaf1d5fb0bfdf778ea5983f40950e91a94331
Syed Jawad <jawadhussaini1804@gmail.com>
2024-06-03 00:52:58 +0500

    Promisees

--- promise.js
+++ promise.js
@@ -1,3 +1,8 @@
+// can't create arrow function, why?
+//how to use .then with function?
+
+
+
 // async function DisplayData() {
 //     let myProfile = new Promise(function(resolve, reject){
 //         setTimeout(()=>{
@@ -21,26 +26,57 @@
 // }
 // Data();
 
-function pro(){
-    let promised = (function(resolve, reject){
-        setTimeout(()=>{
-            if(resolve){
-            console.log("You are on time");
-            }else{
-                console.log(reject) 
-            }
-        },2200)
-    });
-    let promiseDone = promised;
-    console.log(promiseDone(promised))
-};
-pro();
-
-
-// function fetching(){
-//     let data = new Promise(function(resolve){
+// function pro(){
+//     let promised = (function(resolve, reject){
 //         setTimeout(()=>{
-//             if()
-//         })
-//     })
-// }
+//             if(resolve){
+//             console.log("You are on time");
+//             }else{
+//                 console.log(reject) 
+//             }
+//         },2200)
+//     });
+//     let promiseDone = promised;
+//     console.log(promiseDone(promised));
+// };
+// pro();
+
+
+// // function fetching(){
+//     let Data = new Promise(function(resolve,reject){
+//         setTimeout(function(){
+//             console.log("Data start to Fecth");
+//             resolve()
+//         },2000);
+//     });
+// // };
+// Data.then(function(){
+//     console.log("Data has fetched")
+// });
+
+// const obj = new Promise(function(resolve, reject){
+//     setTimeout(()=>{
+//         resolve({username: "Malik", gmail: "malik@123.com"})
+//     },3000)
+// })
+
+// obj.then(function(resolved){
+//     console.log(resolved)
+// })
+
+// Same condition like as writen in above syntax
+
+async function object() {
+    const obj = new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            resolve({username: "Malik", gmail: "malik@123.com"})
+        },3000)
+    })
+    
+    // obj.then(function(resolved){
+    //     console.log(resolved)
+    // })
+    let resolving = await obj;
+    console.log(resolving)
+};
+object()
